cc36cdb25a3da3d92b816f26e49a3532efb0ab54
Le Duc Anh <anhld64@fpt.com>
2024-10-08 15:13:53 +0700

    feat: experiment testing blink.cmp

--- lua/plugins/lazyvim_extras/init.lua
+++ lua/plugins/lazyvim_extras/init.lua
@@ -1,7 +1,6 @@
 return {
   -- { import = "lazyvim.plugins.extras.coding.yanky" },
   -- { import = "lazyvim.plugins.extras.dap.core" },
-  { import = "lazyvim.plugins.extras.editor.mini-files" },
   -- { import = "lazyvim.plugins.extras.lang.cmake" },
   -- { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lsp.neoconf" },
--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,30 +1,75 @@
 return {
-  "hrsh7th/nvim-cmp",
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    local cmp_window = require("cmp.config.window")
-    -- opts.sources = cmp.config.sources({
-    --   { name = "nvim_lsp" },
-    --   { name = "snippets", max_item_count = 3 },
-    --   { name = "buffer", max_item_count = 5, keyword_length = 5 },
-    --   { name = "path", max_item_count = 5 },
-    -- })
-    opts.window = {
-      completion = cmp_window.bordered({ col_offset = -8 }),
-      documentation = cmp_window.bordered(),
-    }
-    opts.formatting.fields = { "kind", "abbr", "menu" }
-    opts.formatting.format = function(_, item)
-      local icons = require("lazyvim.config").icons.kinds
-      local kind = item.kind
-      item.menu = " (" .. kind .. ")"
+  {
+    -- "hrsh7th/nvim-cmp",
+    "yioneko/nvim-cmp",
+    branch = "perf-up",
+    enabled = false,
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local cmp_window = require("cmp.config.window")
+      -- opts.sources = cmp.config.sources({
+      --   { name = "nvim_lsp" },
+      --   { name = "snippets", max_item_count = 3 },
+      --   { name = "buffer", max_item_count = 5, keyword_length = 5 },
+      --   { name = "path", max_item_count = 5 },
+      -- })
+      opts.window = {
+        completion = cmp_window.bordered({ col_offset = -8 }),
+        documentation = cmp_window.bordered(),
+      }
+      opts.formatting.fields = { "kind", "abbr", "menu" }
+      opts.formatting.format = function(_, item)
+        local icons = require("lazyvim.config").icons.kinds
+        local kind = item.kind
+        item.menu = " (" .. kind .. ")"
 
-      if icons[item.kind] then
-        item.kind = " " .. icons[item.kind]
+        if icons[item.kind] then
+          item.kind = " " .. icons[item.kind]
+        end
+        item.dup = { luasnip = 1, buffer = 1, path = 1, nvim_lsp = 0 }
+        return item
       end
-      item.dup = { luasnip = 1, buffer = 1, path = 1, nvim_lsp = 0 }
-      return item
-    end
-  end,
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    lazy = false, -- lazy loading handled internally
+    -- optional: provides snippets for the snippet source
+    dependencies = "rafamadriz/friendly-snippets",
+
+    -- use a release tag to download pre-built binaries
+    version = "v0.*",
+    -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+
+    opts = {
+      keymap = {
+        accept = { "<Tab>", "<CR>" },
+        select_prev = { "<Up>", "<C-j>", "<C-p>" },
+        select_next = { "<Down>", "<C-k>", "<C-p>" },
+      },
+      highlight = {
+        -- sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release, assuming themes add support
+        use_nvim_cmp_as_default = true,
+      },
+      windows = { autocomplete = {
+        draw = "reversed",
+        border = "padded",
+      } },
+
+      signature_help = { enabled = true },
+      -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- adjusts spacing to ensure icons are aligned
+      nerd_font_variant = "mono",
+
+      -- experimental auto-brackets support
+      -- accept = { auto_brackets = { enabled = true } }
+
+      -- experimental signature help support
+      -- trigger = { signature_help = { enabled = true } }
+    },
+  },
 }
